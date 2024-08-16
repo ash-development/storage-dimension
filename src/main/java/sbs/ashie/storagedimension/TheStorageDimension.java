21ffdbd6db03ef5c436fa25e51b7aa96c78a5328
ash <ash@schnoor.email>
2024-08-16 13:57:43 -0400

    🗑️ i give up

--- src/main/java/sbs/ashie/storagedimension/TheStorageDimension.java
+++ src/main/java/sbs/ashie/storagedimension/TheStorageDimension.java
@@ -1,56 +1,69 @@
 package sbs.ashie.storagedimension;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.dimension.v1.FabricDimensionTypes;
+import net.fabricmc.fabric.api.dimension.v1.FabricDimensions;
+import net.fabricmc.fabric.api.object.builder.v1.worldgen.FabricWorldGenerator;
+import net.minecraft.block.Blocks;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
-import net.minecraft.world.dimension.DimensionType;
-import net.minecraft.world.dimension.DimensionTypes;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.DimensionType;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkPos;
+import net.minecraft.world.chunk.WorldChunk;
+import net.minecraft.world.chunk.ChunkSection;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.BiomeCreationSettings;
-import net.minecraft.world.biome.BiomeEffects;
-import net.minecraft.world.biome.BiomeGenerationSettings;
-import net.minecraft.world.biome.SpawnSettings;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.chunk.ChunkGeneratorConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.OptionalLong;
+import net.minecraft.world.gen.feature.DefaultFeatureConfig;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.FeatureConfig;
 
 public class TheStorageDimension implements ModInitializer {
-    public static final Identifier STORAGE_DIMENSION_ID = new Identifier("storagedimension", "storage");
-    public static final Logger LOGGER = LoggerFactory.getLogger("storagedimension");
+    public static final Identifier STORAGE_DIMENSION_ID = new Identifier("storagedimension", "storage_dimension");
 
     @Override
     public void onInitialize() {
-        // Register DimensionType
-        DimensionType storageDimensionType = DimensionType.builder()
-                .fixedTime(OptionalLong.of(6000)) // Fixed time (e.g., midday)
-                .hasSkylight(false)
-                .hasCeiling(false)
-                .ambientLight(1.0F)
-                .natural(false)
-                .ultrawarm(false)
-                .build();
+        // Register the dimension type
+        FabricDimensionTypes.register(
+                STORAGE_DIMENSION_ID,
+                FabricDimensionTypes.create(
+                        new DimensionType(0, false, false, 0, false, false, false, false, false)
+                )
+        );
 
-        DimensionTypes.register(STORAGE_DIMENSION_ID, storageDimensionType);
+        // Register dimension and chunk generator
+        FabricWorldGenerator.registerDimension(
+                STORAGE_DIMENSION_ID,
+                new ChunkGenerator(new ChunkGeneratorConfig() {
+                    @Override
+                    public void fillChunk(World world, ChunkPos pos, ChunkSection section) {
+                        for (int x = 0; x < 16; x++) {
+                            for (int z = 0; z < 16; z++) {
+                                for (int y = 0; y < 256; y++) {
+                                    section.setBlockState(new BlockPos(x, y, z), Blocks.WHITE_CONCRETE.getDefaultState(), false);
+                                }
+                            }
+                        }
+                    }
+                })
+        );
 
-        LOGGER.info("Storage dimension mod initialized!");
-
-        // Register Chunk Generator
-        ChunkGenerator storageChunkGenerator = new StorageChunkGenerator();
-        ChunkGeneratorRegistry.register(STORAGE_DIMENSION_ID, storageChunkGenerator);
-
-        // Register Biome
-        Biome storageBiome = createStorageBiome();
-        BiomeRegistry.register(STORAGE_DIMENSION_ID, storageBiome);
-    }
-
-    private Biome createStorageBiome() {
-        return new Biome.Builder()
-                .generationSettings(new BiomeGenerationSettings.Builder().build())
-                .spawnSettings(new SpawnSettings.Builder().build())
-                .effects(new BiomeEffects.Builder().build())
-                .build();
+        // Register custom biome for the dimension
+        FabricWorldGenerator.registerBiome(
+                STORAGE_DIMENSION_ID,
+                new Biome(new Biome.Settings()
+                        .precipitation(Biome.Precipitation.NONE)
+                        .category(Biome.Category.NONE)
+                        .temperature(0.8F)
+                        .downfall(0.0F)
+                        .effects(new BiomeEffects.Builder()
+                                .skyColor(0xFFFFFF) // White sky
+                                .build())
+                )
+        );
     }
 }
