d7c954b4edfd50ed368de55dfffb4e84984543e8
ash <ash@schnoor.email>
2024-08-16 13:38:18 -0400

    🚧 sigh

--- src/main/java/sbs/ashie/storagedimension/TheStorageDimension.java
+++ src/main/java/sbs/ashie/storagedimension/TheStorageDimension.java
@@ -1,8 +1,8 @@
 package sbs.ashie.storagedimension;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.dimension.DimensionType;
@@ -15,24 +15,26 @@ import java.util.OptionalLong;
 
 public class TheStorageDimension implements ModInitializer {
 	// Use RegistryKey for DimensionType, ChunkGenerator, and Biome
-	public static final RegistryKey<DimensionType> STORAGE_DIMENSION_TYPE_KEY = RegistryKey.of(Registry.DIMENSION_TYPE_KEY, new Identifier("storagedimension", "storage"));
-	public static final RegistryKey<ChunkGenerator> STORAGE_CHUNK_GENERATOR_KEY = RegistryKey.of(Registry.CHUNK_GENERATOR_KEY, new Identifier("storagedimension", "storage"));
-	public static final RegistryKey<Biome> STORAGE_BIOME_KEY = RegistryKey.of(Registry.BIOME_KEY, new Identifier("storagedimension", "storage"));
+	public static final Identifier STORAGE_DIMENSION_ID = new Identifier("storagedimension", "storage");
+	public static final RegistryKey<DimensionType> STORAGE_DIMENSION_TYPE_KEY = RegistryKey.of(Registries.DIMENSION_TYPE_KEY, STORAGE_DIMENSION_ID);
+	public static final RegistryKey<ChunkGenerator> STORAGE_CHUNK_GENERATOR_KEY = RegistryKey.of(Registries.CHUNK_GENERATOR_KEY, STORAGE_DIMENSION_ID);
+	public static final RegistryKey<Biome> STORAGE_BIOME_KEY = RegistryKey.of(Registries.BIOME_KEY, STORAGE_DIMENSION_ID);
 
 	public static final Logger LOGGER = LoggerFactory.getLogger("storagedimension");
 
 	@Override
 	public void onInitialize() {
-		// Create and register DimensionType
-		DimensionType storageDimensionType = DimensionType.create(
-				OptionalLong.of(6000), // Fixed time (e.g., midday)
-				false, // Has skylight
-				false, // Has ceiling
-				1.0F,  // Ambient light
-				false, // Natural
-				false  // Ultrawarm
-		);
+		// Create a custom DimensionType
+		DimensionType storageDimensionType = DimensionType.builder()
+				.fixedTime(OptionalLong.of(6000)) // Fixed time (e.g., midday)
+				.hasSkylight(false)
+				.hasCeiling(false)
+				.ambientLight(1.0F)
+				.natural(false)
+				.ultrawarm(false)
+				.build();
 
+		// Register DimensionType
 		Registry.register(Registries.DIMENSION_TYPE, STORAGE_DIMENSION_TYPE_KEY, storageDimensionType);
 
 		LOGGER.info("Storage dimension mod initialized!");
