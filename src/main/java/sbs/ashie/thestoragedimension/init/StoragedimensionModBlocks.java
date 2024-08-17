8479e4268ba689dfe6c2e3afe5b7f812da400d87
ash <ash@schnoor.email>
2024-08-17 00:17:09 -0400

    :construction: Maybe this'll work?

--- src/main/java/sbs/ashie/thestoragedimension/init/StoragedimensionModBlocks.java
+++ src/main/java/sbs/ashie/thestoragedimension/init/StoragedimensionModBlocks.java
@@ -1,17 +1,18 @@
-
-/*
- *	MCreator note: This file will be REGENERATED on each build.
- */
 package sbs.ashie.thestoragedimension.init;
 
 import sbs.ashie.thestoragedimension.StoragedimensionMod;
+import sbs.ashie.thestoragedimension.block.CustomWhiteConcreteBlock;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.Registry;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.*;
 
 public class StoragedimensionModBlocks {
+	public static final Block CUSTOM_WHITE_CONCRETE = register("custom_white_concrete", new CustomWhiteConcreteBlock(BlockBehaviour.Properties.of().strength(1.8F)));
+
 	public static void load() {
 	}
 
